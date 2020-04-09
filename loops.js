70bcb06410dc66bb5c5060c6f14d4604a7043879
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-09 19:05:57 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,11 +1,12 @@
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
+
 
 
 function doWhileLoop(n) {
   do {
+    function incrementVariable() {
+      i = i + 1;
+      return i;
+    }
     console.log("I run once regardless.");
   } while (incrementVariable() < n);
 }
