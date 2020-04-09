c76cba5fa0098b4de470bce136fc89aa7370e96c
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-09 19:10:23 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,21 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i==1) {
+        array.push(`I am ${i} strange loop.`)
+    } else {
+        array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array;
+}
 
-
+function whileLoop(n) {
+    while (n > 0) {
+            console.log(n);
+            n -= 1;
+          }
+        return 'done';
+      }
 
 function doWhileLoop(n) {
   let i = 0;
