789713c782e3aba51f7211913738ed70321c19cf
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-09 18:31:40 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -10,18 +10,3 @@ function forLoop(array) {
 }
 
 
-function whileLoop(n) {
-    while (n>0) {
-        if (n == 10) {
-          while (n > 10) {
-            console.log("I run once regardless");
-            n -= 1;
-          }
-        else {
-            n -= 1;
-          }
-        }
-      }
-        console.log("I run once regardless");
-        return 'done';
-    }
