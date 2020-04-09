f4174c13719bbdcc0f6a8b1cfa983cc4927a9a18
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-09 18:51:19 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function whileLoop(n) {
-    while (n>0) {
+    while (n > -1) {
         if (n == 10) {
-          while (n > 0) {
+          while (n > -1) {
             console.log("I run once regardless");
             n -= 1;
           }
